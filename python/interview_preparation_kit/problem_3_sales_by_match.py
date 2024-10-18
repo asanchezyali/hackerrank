2da2cc87403193ebb72349190052af0e1cfb4539
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-10-17 21:35:38 -0500

    🛰️ Add Sock Merchant problem and solution

---
+++ python/interview_preparation_kit/problem_3_sales_by_match.py
@@ -0,0 +1,38 @@
+import time
+
+def sockMerchant(n, ar):
+    sock_count = {}
+    matching_pairs = 0
+    for i in range(n):
+        if ar[i] in sock_count:
+            sock_count[ar[i]] += 1
+            if sock_count[ar[i]] % 2 == 0:
+                matching_pairs += 1
+        else:
+            sock_count[ar[i]] = 1
+    return matching_pairs
+
+# This is the first solution I came up with. It is not the most efficient one, but it works.
+""" 
+def sockMerchant(n, ar):
+    matching_pairs = 0
+    for i in range(n):
+        if ar[i] !=0:
+            for j in range(i+1, n):
+                if ar[i] == ar[j]:
+                    matching_pairs += 1
+                    ar[j] = 0
+                    break
+    return matching_pairs
+"""
+
+if __name__ == "__main__":
+    start_time = time.time()
+    n = 9
+    ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    result = sockMerchant(n, ar)
+    elapsed_time = time.time() - start_time
+    print(f"--- {elapsed_time:.4f} seconds ---")
+
+
+
